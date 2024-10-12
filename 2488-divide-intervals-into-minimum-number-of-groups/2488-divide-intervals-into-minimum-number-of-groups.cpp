754a28ebd1752924e35dd06190ce9c7d3024fb4c
gaintant <59084124+gaintant@users.noreply.github.com>
2024-10-12 13:26:53 +0530

    Time: 247 ms (87.16%), Space: 92.6 MB (93.47%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -3,15 +3,13 @@ public:
     int minGroups(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
         priority_queue<int, vector<int>, greater<int>> pq;
-        int ans = 1, n = intervals.size();
+        int n = intervals.size();
         pq.push(intervals[0][1]);
         for(int i=1; i<n; i++){
             if(pq.top() < intervals[i][0])
                 pq.pop();
-            else
-                ans++;
             pq.push(intervals[i][1]);
         }
-        return ans;
+        return pq.size();
     }
 };
