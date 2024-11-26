a8e5d967a13fd9ebf594058534a728f0518c2a4c
gaintant <59084124+gaintant@users.noreply.github.com>
2024-11-26 09:04:13 +0530

    Time: 31 ms (55.14%), Space: 104.2 MB (38.58%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.cpp
+++ 3189-find-champion-ii/3189-find-champion-ii.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    int findParent(vector<int> &parent, int j){
-        if(parent[j] == j)
-            return j;
-        return parent[j] = findParent(parent, parent[j]);
-    }
-    void union_find(vector<int> &parent, int i, int j){
-        parent[findParent(parent, j)] = findParent(parent, i);
-
-    }
     int findChampion(int n, vector<vector<int>>& edges) {
         vector<bool> parent(n, false);
+        int c = 0, ans = -1;
         for(auto edge : edges){
             parent[edge[1]] = true;
         }
-        int c = 0, ans = -1;
         for(int i=0; i<n; i++)
             if(parent[i] == false){
                 c++;
