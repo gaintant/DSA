4f8558cc02049aaa7a0a16115fb89cc2e64946de
gaintant <59084124+gaintant@users.noreply.github.com>
2024-12-08 11:23:24 +0530

    Time: 200 ms (15.76%), Space: 149.4 MB (39.67%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
@@ -5,6 +5,8 @@ public:
         stack<vector<int>> st;
         int n = events.size(), currMA;
         int ans = 0;
+        // using stack to implement upper bound for start time
+        //i.e. for time >= t what is the max v we can use
         for(int i= n-1; i>=0; i--){
             auto event = events[i];
             if(st.empty() || event[2] > st.top()[1]){
@@ -16,9 +18,10 @@ public:
         auto comparator = [](const vector<int>& a, const vector<int>& b) {
             return a[1] < b[1]; 
         };
-
+        // sorting according to ending time
         sort(events.begin(), events.end(), comparator);
         for(auto event : events){
+            // remove the stack event which started before event started 
             while(!st.empty() && st.top()[0] <= event[1]){
                 st.pop();
             }
