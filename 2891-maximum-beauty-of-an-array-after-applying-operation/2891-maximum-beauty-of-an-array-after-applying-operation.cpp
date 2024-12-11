087519925ab78c04b225ddfad3038b3a5cfe0215
gaintant <59084124+gaintant@users.noreply.github.com>
2024-12-11 23:06:20 +0530

    Time: 50 ms (63.14%), Space: 105 MB (67.7%) - LeetHub

---
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int i=0, j=1, n = nums.size();
+        int ans = 1;
+        for(int i=0; i<n;i++){
+            while(j<n && nums[j] <= nums[i] +2*k)
+                j++;
+            ans = max(ans, j-i);
+        }
+        return ans;
+    }
+};
