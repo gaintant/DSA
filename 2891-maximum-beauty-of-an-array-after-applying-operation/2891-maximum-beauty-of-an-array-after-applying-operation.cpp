7c14f69cb2add8dc579f7fce9ebc9d328811ff75
gaintant <59084124+gaintant@users.noreply.github.com>
2024-12-11 23:06:45 +0530

    Time: 40 ms (86.1%), Space: 104.9 MB (94.06%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(), nums.end());
         int i=0, j=1, n = nums.size();
         int ans = 1;
-        for(int i=0; i<n;i++){
+        for(int i=0; j<n;i++){
             while(j<n && nums[j] <= nums[i] +2*k)
                 j++;
             ans = max(ans, j-i);
