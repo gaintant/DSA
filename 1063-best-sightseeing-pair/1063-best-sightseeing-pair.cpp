0455a2128644355c08f2467da949955b0588f95f
gaintant <59084124+gaintant@users.noreply.github.com>
2024-12-27 08:55:42 +0530

    Time: 0 ms (100%), Space: 45.6 MB (55.58%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        int maxVal = -1e5;
+        int maxVal = values[0];
         int n = values.size();
         int ans = INT_MIN;
-        for(int i=0; i<n; i++){
+        for(int i=1; i<n; i++){
             ans = max(ans, maxVal + values[i] - i);
             maxVal = max(maxVal, values[i] + i);
         }
