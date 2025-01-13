a2cce010aef8f72eaa3085360b12f6638c4b9d72
gaintant <59084124+gaintant@users.noreply.github.com>
2025-01-13 08:21:43 +0530

    Time: 20 ms (61.29%), Space: 32.1 MB (58.06%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -3,10 +3,10 @@ public:
     int minimumLength(string s) {
         vector<int> arr(26, 0);
         int ans =0;
-        for(auto ch : s){
+        for(auto &ch : s){
             arr[ch -'a']++;
         }
-        for(auto v : arr){
+        for(auto &v : arr){
             if(v != 0)
                 ans += ((v%2 == 1)? 1 : 2);
         }
