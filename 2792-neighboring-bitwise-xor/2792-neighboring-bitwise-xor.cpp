7ce26cfb197cf1151b9b56fb2af95fdcbead82d2
gaintant <59084124+gaintant@users.noreply.github.com>
2025-01-17 08:34:32 +0530

    Time: 3 ms (59.81%), Space: 264.1 MB (28.04%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -5,12 +5,6 @@ public:
         for(auto d : derived){
             s = d^s;
         }
-        if(s == 0) return true;
-        // s = 1;
-        // for(auto d : derived){
-        //     s = d^s;
-        // }
-        // if(s == 1) return true;
-        return false;
+        return s == 0;
     }
 };
