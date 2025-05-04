d578bd46d8aafe53a9167f152d0c8064a8d199f6
gaintant <59084124+gaintant@users.noreply.github.com>
2025-05-04 11:26:33 +0530

    Time: 20 ms (8.75%), Space: 30.7 MB (8.75%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
         map<vector<int>, int> mp;
-        int ans = 0;
+        int ans = 0, l , h;
         for(auto dominoe : dominoes){
-            int l = min(dominoe[0], dominoe[1]);
-            int h = max(dominoe[0], dominoe[1]);
+            l = min(dominoe[0], dominoe[1]);
+            h = max(dominoe[0], dominoe[1]);
             ans += mp[{l,h}]++;
         }
         return ans;
