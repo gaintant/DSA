673600c942b46c224794ebae63d4dad9f71bb10a
gaintant <59084124+gaintant@users.noreply.github.com>
2025-05-04 11:29:03 +0530

    Time: 12 ms (21.75%), Space: 28.6 MB (12.25%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -4,9 +4,9 @@ public:
         vector<int> mp(100);
         int ans = 0, l , h;
         for(auto dominoe : dominoes){
-            l = min(dominoe[0], dominoe[1]);
-            h = max(dominoe[0], dominoe[1]);
-            ans += mp[l*10 + h]++;
+            // l = min(dominoe[0], dominoe[1]);
+            // h = max(dominoe[0], dominoe[1]);
+            ans += mp[min(dominoe[0], dominoe[1])*10 + max(dominoe[0], dominoe[1])]++;
         }
         return ans;
     }
