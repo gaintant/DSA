26c77818e78dd8acd4034044ad7089c42b0525fb
gaintant <59084124+gaintant@users.noreply.github.com>
2025-06-24 14:54:22 +0530

    Time: 49 ms (5.18%), Space: 19.2 MB (81.8%) - LeetHub

---
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumDeletions(string word, int k) {
+        vector<int> freq(26,0);
+        for(auto ch : word){
+            freq[ch - 'a']++;
+        }
+        sort(freq.begin(), freq.end());
+        for(auto ch : freq)
+            cout<<ch<<" ";
+        int ans = INT_MAX;
+        for(int i=0; i<26; i++){
+            int val = freq[i] + k,p = 0 ;
+            for(int j=0; j<26; j++){
+                if(freq[j] == 0)
+                    continue;
+                if(freq[j] < freq[i])
+                    p += freq[j];
+                
+                else if(freq[j] > val)
+                    p += freq[j] - val;
+            }
+            ans = min(p, ans);
+        }
+        return ans;
+    }
+};
