25a6cc790952b3af7a74dc6586a2ae9c9b9142e2
gaintant <59084124+gaintant@users.noreply.github.com>
2025-06-24 14:55:59 +0530

    Time: 3 ms (84.51%), Space: 19.3 MB (71.06%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -8,8 +8,8 @@ public:
         sort(freq.begin(), freq.end());
         int ans = INT_MAX;
         for(int i=0; i<26; i++){
-            // if(i == 0)
-            //     continue;
+            if(freq[i] == 0)
+                continue;
             int val = freq[i] + k,p = 0 ;
             for(int j=0; j<26; j++){
                 if(freq[j] == 0)
