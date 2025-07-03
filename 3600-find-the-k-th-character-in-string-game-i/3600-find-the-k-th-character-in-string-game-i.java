109e5f773c5e14098b342d478908460af37c688c
gaintant <59084124+gaintant@users.noreply.github.com>
2025-07-03 17:32:47 +0530

    Time: 0 ms (100%), Space: 41.4 MB (91.15%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.java
@@ -1,11 +1,11 @@
 class Solution {
     public char kthCharacter(int k){
         k--;
-        int curr = 0;
+        char curr = 'a';
         for(int i=1; i<512; i *= 2){
             if((k & i) != 0)
                 curr++;
         }
-        return (char)(curr + 'a');
+        return curr;
     }
 }
